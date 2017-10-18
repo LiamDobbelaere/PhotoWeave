8509e21ed7a563c08af94f479f3033b697d4a2fd
DESKTOP-TM28QAC\wardp <ward.pauwels@student.howest.be>
2017-10-18 14:38:49 +0200

    Starting GUI

--- src/main/java/be/howest/photoweave/controllers/EditPhoto.java
+++ src/main/java/be/howest/photoweave/controllers/EditPhoto.java
@@ -1,24 +1,29 @@
 package be.howest.photoweave.controllers;
 
+import be.howest.photoweave.model.imaging.MonochromeImage;
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class EditPhoto {
-    private String path;
-    Image img;
-    Image newImage;
     @FXML
     ImageView photoview;
     @FXML
-    Slider quantizationscale;
+    Slider slider;
     @FXML
     VBox imagevbox;
     @FXML
@@ -32,16 +37,52 @@ public class EditPhoto {
     @FXML
     TextField heightinputtextfield;
 
+    //Image parameters
+    private String path;
+    double imageWidth;
+    double imageHeight;
+    String filename;
+    BufferedImage img;
+    MonochromeImage monochromeImg;
+    Image endImage;
 
-    public void initData(String path) {
+    //Edit parameters
+    int posterizeScale;
+
+
+    public void initData(String path) throws IOException {
+        //init parameters
         this.path = path;
-        img = new Image("file:" + path);
-        photoview.setImage(img);
-        fileNameId.setText("File: " + img.getUrl().substring(img.getUrl().lastIndexOf('/') + 1, img.getUrl().length()));
-        imageSizeId.setText("Width: " + img.getWidth() + "px; Height: " + img.getHeight() + "px;");
-        widthinputtextfield.setText(String.valueOf(img.getWidth()));
-        heightinputtextfield.setText(String.valueOf(img.getHeight()));
-        zoomPhoto(img);
+        this.img = ImageIO.read(new File(path));
+        this.monochromeImg = new MonochromeImage(img);
+        this.imageWidth = img.getWidth();
+        this.imageHeight = img.getHeight();
+        this.filename = path.substring(path.lastIndexOf('/') + 1, path.length());
+        this.posterizeScale = 10;
+
+        //set properties
+        fileNameId.setText("File: " + filename);
+        imageSizeId.setText("Width: " + imageWidth + "px; Height: " + imageHeight + "px;");
+        widthinputtextfield.setText(String.valueOf(imageWidth));
+        heightinputtextfield.setText(String.valueOf(imageHeight));
+
+        updateImage();
+
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                posterizeScale = new_val.intValue();
+                updateImage();
+            }
+        });
+        widthinputtextfield.textProperty().addListener((observable, oldValue, newValue) -> {
+            imageWidth = Integer.parseInt(newValue);
+            updateImage();
+        });
+        heightinputtextfield.textProperty().addListener((observable, oldValue, newValue) -> {
+            imageHeight = Integer.parseInt(newValue);
+            updateImage();
+        });
     }
 
     private void zoomPhoto(Image img) {
@@ -50,10 +91,6 @@ public class EditPhoto {
         } else {
             photoview.setFitWidth(anchorpane.getWidth());
         }
-    }
-
-    public void sliderMoving() {
-        System.out.println(quantizationscale.getValue());
     }
 
     public void zoomin() {
@@ -70,11 +107,22 @@ public class EditPhoto {
     public void sizeChanged(Observable a, String b, String c) {
         double height = Double.parseDouble(b);
         double width = Double.parseDouble(c);
-        img = new Image(path, Double.parseDouble(widthinputtextfield.getText()), Double.parseDouble(heightinputtextfield.getText()), true, true);
-        updateImageView();
+        //newImage = new Image(path, Double.parseDouble(widthinputtextfield.getText()), Double.parseDouble(heightinputtextfield.getText()), true, true);
+        //updateImageView(newImage);
     }
 
-    public void updateImageView(){
-        photoview.setImage(img);
+    public void updateImage() {
+        monochromeImg.setLevels(posterizeScale);
+        monochromeImg.redraw();
+
+
+
+        endImage = SwingFXUtils.toFXImage(monochromeImg.getModifiedImage(), null);
+        updateImageView(endImage);
+    }
+
+    public void updateImageView(Image image) {
+        photoview.setImage(image);
+        zoomPhoto(image);
     }
 }
