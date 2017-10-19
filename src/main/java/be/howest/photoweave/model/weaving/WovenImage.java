90a546ed40b2885d8aafa2e9b78371d9c46ab99a
QuintenDegraeve <Quinten.Degraeve@student.howest.be>
2017-10-19 12:07:05 +0200

    Refactored WomenImage [Weaving]
    + Binding class that contains the image of a binding/pattern.
    + BindingFactory loads in all internal bindings.
    + BindingPalette contains and combines the colors of an image and the
    bindings.
    + ImageUtil contains general functions that are used on BufferedImages.
    
    * Cleaned WomenUmage.
    * Changed WomenImageTest to work with the refactored code.

--- src/main/java/be/howest/photoweave/model/weaving/WovenImage.java
+++ src/main/java/be/howest/photoweave/model/weaving/WovenImage.java
@@ -1,88 +1,24 @@
 package be.howest.photoweave.model.weaving;
 
+import be.howest.photoweave.model.binding.Binding;
+import be.howest.photoweave.model.binding.BindingPalette;
+import be.howest.photoweave.model.util.ImageUtil;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
-/**
- * Created by tomdo on 6/10/2017.
- */
 public class WovenImage {
     private BufferedImage sourceImage;
     private BufferedImage resultImage;
-    private HashMap<Integer, Integer> indexedColors;
-    private BufferedImage[] patterns;
-    //private int[][] patterns;
+
+    private BindingPalette bindingPalette;
 
     public WovenImage(BufferedImage sourceImage) {
-        if (sourceImage.getType() != BufferedImage.TYPE_INT_RGB)
-        {
-            BufferedImage convertedImg = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-            Graphics graphics = convertedImg.getGraphics();
-            graphics.drawImage(sourceImage, 0, 0, null);
-            graphics.dispose();
+        this.sourceImage = ImageUtil.convertImageToRGBInt(sourceImage);
 
-            sourceImage = convertedImg;
-        }
-
-        this.sourceImage = sourceImage;
-        this.resultImage = new BufferedImage(this.sourceImage.getWidth(), this.sourceImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-        this.indexedColors = new HashMap<>();
-
-        indexColors();
-    }
-
-    private void indexColors() {
-        List<Integer> sortedColors = new ArrayList<>();
-
-        //this.indexedColors.clear();
-
-        DataBufferInt dbb = (DataBufferInt) this.sourceImage.getRaster().getDataBuffer();
-        int[] imageData = dbb.getData();
-
-        for (int rgb : imageData) {
-            //if (!this.indexedColors.containsKey(rgb))
-            //    this.indexedColors.put(rgb, this.indexedColors.size());
-
-            if (!sortedColors.contains(rgb))
-                sortedColors.add(rgb);
-        }
-
-        sortedColors.sort(Collections.reverseOrder());
-
-        for (int i = 0; i < sortedColors.size(); i++) {
-            int rgb = sortedColors.get(i);
-            this.indexedColors.put(rgb, i);
-        }
-
-        this.patterns = new BufferedImage[this.indexedColors.size()];
-    }
-
-    public BufferedImage getResultImage() {
-        return resultImage;
-    }
-
-    public HashMap<Integer, Integer> getIndexedColors() {
-        return indexedColors;
-    }
-
-    public void setPattern(int index, BufferedImage pattern) {
-        if (pattern.getType() != BufferedImage.TYPE_INT_RGB)
-        {
-            BufferedImage convertedImg = new BufferedImage(pattern.getWidth(), pattern.getHeight(), BufferedImage.TYPE_INT_RGB);
-            Graphics graphics = convertedImg.getGraphics();
-            graphics.drawImage(pattern, 0, 0, null);
-            graphics.dispose();
-
-            pattern = convertedImg;
-        }
-
-        this.patterns[index] = pattern; //((DataBufferInt) pattern.getRaster().getDataBuffer()).getData();
+        this.resultImage = ImageUtil.createBlankCopy(this.sourceImage);
+        this.bindingPalette = new BindingPalette(this.sourceImage);
     }
 
     public void redraw() {
@@ -90,21 +26,10 @@ public class WovenImage {
         gr.clearRect(0, 0, this.resultImage.getWidth(), this.resultImage.getHeight());
         gr.dispose();
 
-        DataBufferInt dbi = (DataBufferInt) this.sourceImage.getRaster().getDataBuffer();
-        int[] imageData = dbi.getData();
+        int[] imageData = ImageUtil.getDataBufferIntData(this.sourceImage);
+        int[] targetData = ImageUtil.getDataBufferIntData(this.resultImage);
 
-        DataBufferInt dbb = (DataBufferInt) this.resultImage.getRaster().getDataBuffer();
-        int[] targetData = dbb.getData();
-
-        //Tiling algorithm
-        for (int i = 0; i < imageData.length; i++) {
-            BufferedImage pattern = this.patterns[this.indexedColors.get(imageData[i])];
-
-            int x = (i % this.sourceImage.getWidth()) % pattern.getWidth();
-            int y = ((int) Math.floor(i / this.sourceImage.getWidth())) % pattern.getHeight();
-
-            targetData[i] = pattern.getRGB(x, y); //pattern[x + (y * 10)]; //pattern.getRGB(x, y); //agh[x + (y * 10)];
-        }
+        tilingAlgorithm(imageData,targetData);
     }
 
     public boolean hasFloaters() {
@@ -122,15 +47,29 @@ public class WovenImage {
             else colorCount = 0;
 
             if (colorCount > 3) {
-                //targetData[i] = Color.red.getRGB();
-
                 hasFloaters = true;
-                //colorCount = 0;
             }
 
             lastColor = color;
         }
 
         return hasFloaters;
+    }
+
+    private void tilingAlgorithm(int[] imageData, int[] targetData){
+        for (int i = 0; i < imageData.length; i++) {
+            Binding binding = bindingPalette.getBindingPalette().get(imageData[i]);
+            BufferedImage pattern = binding.getBindingImage();
+
+            int x = (i % this.sourceImage.getWidth()) % pattern.getWidth();
+            int y = ((int) Math.floor(i / this.sourceImage.getWidth())) % pattern.getHeight();
+
+            targetData[i] = pattern.getRGB(x, y); //binding[x + (y * 10)]; //binding.getRGB(x, y); //agh[x + (y * 10)];
+        }
+    }
+
+    /* Getter */
+    public BufferedImage getResultImage() {
+        return resultImage;
     }
 }
