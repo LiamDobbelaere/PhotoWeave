f1365c4831d1907b469bda6b718da667be298223
QuintenDegraeve <Quinten.Degraeve@student.howest.be>
2017-11-06 15:56:01 +0100

    BindingMaker with preview

--- src/main/java/be/howest/photoweave/components/BindingMaker.java
+++ src/main/java/be/howest/photoweave/components/BindingMaker.java
@@ -1,81 +1,107 @@
 package be.howest.photoweave.components;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class BindingMaker {
 
     private static final int PIXEL_SIZE = 40;
-    private static final int W = 800;
-    private static final int H = 600;
+    private static final int X_TILES = 10;
+    private static final int Y_TILES = 10;
+
+    private static final int W = X_TILES * PIXEL_SIZE;
+    private static final int H = Y_TILES * PIXEL_SIZE;
 
     //private static final int X_TILES = W / PIXEL_SIZE;
     //private static final int Y_TILES = H / PIXEL_SIZE;
 
-    private static final int X_TILES = 10;
-    private static final int Y_TILES = 10;
-    @FXML
-    public ImageView imageview;
-    public Pane bindingpane;
+    private ObservableList<Integer> sizes = FXCollections.observableArrayList();
 
     private Pixel[][] grid = new Pixel[X_TILES][Y_TILES];
-    private Scene scene;
 
-    public BindingMaker() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("components/BindingMaker.fxml"));
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
-        fxmlLoader.load();
+    public JFXComboBox bindingsizes;
+    public Pane bindingpane;
+    public Pane imageviewpane;
 
-        bindingpane
+
+    public BindingMaker() {
     }
 
-    public void initialize(Stage stage) throws Exception {
-        //scene = new Scene(createGrid());
-        //createGrid();
-        //stage.setScene(scene);
-        //stage.show();
+    public void init() {
+        sizes.addAll(5, 10);
+        bindingsizes.setItems(sizes);
+        bindingsizes.getSelectionModel().selectFirst();
+        createGrid();
     }
 
-    private Parent createGrid() {
-        Pane root = new Pane();
-        root.setPrefSize(W, H);
+    private void createGrid() {
+        bindingpane.setPrefSize(W, H);
+        bindingpane.setVisible(true);
 
         for (int y = 0; y < Y_TILES; y++) {
             for (int x = 0; x < X_TILES; x++) {
                 Pixel pixel = new Pixel(x, y, false, true);
 
                 grid[x][y] = pixel;
-                root.getChildren().add(pixel);
+                bindingpane.getChildren().add(pixel);
             }
         }
-        //bindingpane = root;
-        return root;
     }
 
-    private void export(){
+    @FXML
+    private void preview() {
         BufferedImage bufferedImage = new BufferedImage(10, 10,
                 BufferedImage.TYPE_INT_RGB);
 
-        int rgb = 0xFF00FF00;
-        for (int index1 = 0; index1 < grid.length; index1++){
-            for (int index2 = 0; index2 < grid[0].length; index2++){
-                if (grid[index1][index2].isFilled)bufferedImage.setRGB(index1, index2, rgb);
+        int rgb = 0xFFFFFFFF;
+        for (int index1 = 0; index1 < grid.length; index1++) {
+            for (int index2 = 0; index2 < grid[0].length; index2++) {
+                if (grid[index1][index2].isFilled) bufferedImage.setRGB(index1, index2, rgb);
             }
         }
-        imageview.setImage(SwingFXUtils.toFXImage(bufferedImage, null));
+
+        Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+        ImagePattern imagePattern = new ImagePattern(image);
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                Preview pr = new Preview(i, j, imagePattern);
+
+                imageviewpane.getChildren().add(pr);
+            }
+        }
+    }
+
+    public void export(ActionEvent actionEvent) {
+    }
+
+    private class Preview extends StackPane {
+        private int x, y;
+        private Rectangle rectangle;
+
+        public Preview(int x, int y, ImagePattern imagePattern) {
+            this.x = x;
+            this.y = y;
+            this.rectangle = new Rectangle(10, 10);
+            this.rectangle.setFill(imagePattern);
+
+            getChildren().addAll(rectangle);
+
+            setTranslateX(x * 10);
+            setTranslateY(y * 10);
+        }
     }
 
     private class Pixel extends StackPane {
@@ -83,11 +109,11 @@ public class BindingMaker {
         private boolean isFilled;
         private Rectangle rectangle;
 
-        public Pixel(int x, int y, boolean isFilled, boolean hasStroke) {
+        Pixel(int x, int y, boolean isFilled, boolean hasStroke) {
             this.x = x;
             this.y = y;
             this.isFilled = isFilled;
-            this.STROKE_SIZE = (hasStroke)? 2 : 0;
+            this.STROKE_SIZE = (hasStroke) ? 2 : 0;
             this.rectangle = new Rectangle(PIXEL_SIZE - STROKE_SIZE, PIXEL_SIZE - STROKE_SIZE);
 
             rectangle.setFill(Color.BLACK);
@@ -100,11 +126,9 @@ public class BindingMaker {
             setOnMouseClicked(e -> fill());
         }
 
-        public void fill(){
-            //rectangle.setFill(Color.RED);
+        void fill() {
             this.isFilled = !this.isFilled;
-            rectangle.setFill((isFilled)? Color.WHITE : Color.BLACK);
-            export();
+            rectangle.setFill((isFilled) ? Color.WHITE : Color.BLACK);
         }
     }
 }
